a4324922b6b78e4adba6c2b898a8a47f679a00dd
Russel Dave Cruz <yamitsuki28@gmail.com>
2021-07-19 17:06:49 +0800

    api builder

--- utils/api.py
+++ utils/api.py
@@ -1,14 +1,27 @@
 import requests
+import validators
 
 class Api:
-    def get(self, uri, parameters):
-        return requests.get(self.build(uri, parameters))
+    def get(self, link=None, parameters=None):
+        return requests.get(self.build(link, parameters))
     
-    def build(self, uri, parameters):
-        return uri + "?" + "&".join(self.parse_parameters(parameters))
+    def build(self, link = None, parameters = None):
+        link = self.check_url(link)
+        if parameters is None:
+            return link
+        return link + "?" + self.parse_parameters(parameters)
 
     def parse_parameters(self, parameters):
+        if type(parameters) is not dict:
+            raise TypeError("The parameter must be a dictionary")
         keys = []
         for key, value in parameters.items():
             keys.append(key + "=" + value)
-        return keys
+        return "&".join(keys)
+
+    def check_url(self, link):
+        if type(link) is not str:
+            raise TypeError("The link must be a string")
+        if not validators.url(link):
+            raise ValueError("The link must be a valid URL")
+        return link
