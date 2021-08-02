b8774399c99295ea356465d1336c12a3c3f3c836
Russel Dave Cruz <yamitsuki28@gmail.com>
2021-08-02 17:21:18 +0800

    save pandas data to database

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from helper.facebook import Facebook
 from dataframe.facebook_insight import FacebookInsight
+from utils.database import Database
 import sys
 
 if __name__ == "__main__":
@@ -21,5 +22,9 @@ if __name__ == "__main__":
 
     filtered = data.reindex(columns=["date_start", "campaign_name", "impressions", "reach", "inline_link_clicks", "ctr", "cpc", "spend", "roas", "purchase", "revenue"])
     output = filtered.rename(columns={"date_start": "date", "campaign_name": "name", "inline_link_clicks": "clicks"}, errors="raise")
+
+    connection = Database(database="example").initialize()
+
+    output.to_sql("demo", con=connection, if_exists="append", index=False)
 
     print(output)
